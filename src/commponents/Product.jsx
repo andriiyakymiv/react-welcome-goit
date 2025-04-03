cae1f038fc0c4637e042a831cfac3220e6cbe11e
andrii <a.iakumiv@gmail.com>
2025-04-03 20:41:39 +0300

    Refactor Product component to accept props for dynamic product names

--- src/commponents/Product.jsx
+++ src/commponents/Product.jsx
@@ -1,21 +1,8 @@
-export default function Product() {
-	const price = 999;
-	const imgUrl = "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
-
-  return (
-    <>
-      <h2>Tacos</h2>
-	  <img src={imgUrl} alt="Tacos With Lime" width="640" />
-      <p>Price: {price} credits</p>
-    </>
-  );
-};
-
-export  function Products() {
+export default function Product(props) {
   return (
     <div>
-      <h2>Tacos</h2>
-	  <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640" alt="Tacos With Lime" width="640" />
+      <h2>{props.name}</h2>
+	  <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640" alt="Tacos With Lime" width="480" />
 	  <p>Price: 999 credits</p>
     </div>
   );
