70b269ccf545c969858b6d32385fc44f7072424b
andrii <a.iakumiv@gmail.com>
2025-04-03 20:43:20 +0300

    Update Product component to use dynamic image URL and price props

--- src/commponents/Product.jsx
+++ src/commponents/Product.jsx
@@ -2,8 +2,8 @@ export default function Product(props) {
   return (
     <div>
       <h2>{props.name}</h2>
-	  <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640" alt="Tacos With Lime" width="480" />
-	  <p>Price: 999 credits</p>
+	  <img src={props.imgUrl} alt={props.name} width="480" />
+	  <p>Price: {props.price} credits</p>
     </div>
   );
 };
