e51bf7da281df9bdb0cdc56c5d642c0933071bc1
andrii <a.iakumiv@gmail.com>
2025-04-04 19:58:44 +0300

    Refactor Mailbox component to use descriptive prop names and improve unread messages display

--- src/commponents/messages/messages.jsx
+++ src/commponents/messages/messages.jsx
@@ -1,10 +1,12 @@
-const Mailbox = ({ username, messages }) => {
+const Mailbox = ({ name, unreadMessages }) => {
   return (
-    <>
-      <p>Hello {username}</p>
-      {messages.length > 0 && (
-        <p>You have {messages.length} unread messages</p>
-      )}
-    </>
+    <div>
+      <p>Hello {name}</p>
+      <p>
+        {unreadMessages.length > 0
+          ? `You have ${unreadMessages.length} unread messages`
+          : "No unread messages"}
+      </p>
+    </div>
   );
 };
